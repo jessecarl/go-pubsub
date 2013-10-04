77a5cbf1d2666eb3f5d5847c1d25cefe9a2e545a
Jesse Allen <jesse@jessecarl.com>
2013-10-04 11:32:28 -0700

    Add test stub
    Sets build status as failing because the tests have yet to be
    written.

---
+++ pubsub_test.go
@@ -0,0 +1,17 @@
+// Copyright 2012 Jesse Allen. All rights reserved.
+// Use of this source code is governed by a BSD-style
+// license that can be found in the LICENSE file.
+
+package pubsub
+
+import (
+  "testing"
+)
+
+func TestNew(t *testing.T) {
+  // nothing to test.
+}
+
+func TestRegister(t *testing.T) {
+  t.Errorf("Tests not created")
+}
