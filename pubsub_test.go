9b8617e531c19b13f4020d0540fb7f717d2fee2a
Jesse Allen <jesse@jessecarl.com>
2013-10-08 13:52:27 -0700

    Test created for `New()`
    Tests that the pointer returned is non-nil.

--- pubsub_test.go
+++ pubsub_test.go
@@ -5,13 +5,17 @@
 package pubsub
 
 import (
-  "testing"
+	"testing"
 )
 
 func TestNew(t *testing.T) {
-  // nothing to test.
+	// almost too dumb to test, but we will anyhow
+	ps := New()
+	if ps == nil {
+		t.Errorf("New() returned nil pointer")
+	}
 }
 
 func TestRegister(t *testing.T) {
-  t.Errorf("Tests not created")
+	t.Errorf("Tests not created")
 }
